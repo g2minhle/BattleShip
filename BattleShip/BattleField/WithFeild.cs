45c7643ec5c5fd260dafef19011c87e7caa8b0be
g2minhle_cp <lehoangminh@live.com>
2014-06-20 05:36:27 +0000

    Fix build files

---
+++ BattleShip/BattleField/WithFeild.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+using System.Media;
+namespace BattleField
+{
+    public class WithFeild : BattleField
+    {
+        public WithFeild()
+        { }
+        public delegate void HittedHandel(int i, int j);
+        public event HittedHandel Hitted;
+        public delegate void MissedHandel(int i, int j);
+        public event MissedHandel Missed;
+        public delegate void ShipDestroyedHandel(int number, Player.Point head, Player.Point tail, int direction);
+        public event ShipDestroyedHandel Destroyed;
+        protected void GetField(Player.APlayer field)
+        {
+            base.New(field.Height, field.Width);
+            player = field;
+            CreatePlayerEvent();
+        }
+        protected override void player_Destroyed(int number, Player.Point head, Player.Point tail, int direction)
+        {
+            try
+            {
+                Destroyed(number, head, tail, direction);
+            }
+            catch { }
+        }
+        protected override void player_Hitted(int i, int j)
+        {
+            try
+            {
+                base.player_Hitted(i, j);
+                Hitted(i, j);
+            }
+            catch { }
+        }
+    }
+}
--- BattleShip/Const/Const.cs
+++ BattleShip/Const/Const.cs
@@ -23,14 +23,16 @@ namespace Const
         public static Color WaterColor { get { return Color.Aquamarine; } }
         public static Color ShootedWaterColor { get { return Color.Blue; } }
 
-        public static string MissedSound { get { return "Sound\\IntoWater.wav"; } }
-        public static string SeaSound { get { return "Sound\\Sea.wav"; } }
-        public static string BumSound { get { return "Sound\\Destroyed.wav"; } }
-        public static string FireSound { get { return "Sound\\Fire.wav"; } }
-        public static string GunSound1 { get { return "Sound\\GunFire1.wav"; } }
-        public static string GunSound2 { get { return "Sound\\GunFire2.wav"; } }
-        public static string WinSound{ get { return "Sound\\Win.wav"; } }
-        public static string LoseSound { get { return "Sound\\Lose.wav"; } } 
+        public static string TheFire { get { return "Resources\\fire.bmp"; } }
+
+        public static string MissedSound { get { return "Resources\\Sound\\IntoWater.wav"; } }
+        public static string SeaSound { get { return "Resources\\Sound\\Sea.wav"; } }
+        public static string BumSound { get { return "Resources\\Sound\\Destroyed.wav"; } }
+        public static string FireSound { get { return "Resources\\Sound\\Fire.wav"; } }
+        public static string GunSound1 { get { return "Resources\\Sound\\GunFire1.wav"; } }
+        public static string GunSound2 { get { return "Resources\\Sound\\GunFire2.wav"; } }
+        public static string WinSound { get { return "Resources\\Sound\\Win.wav"; } }
+        public static string LoseSound { get { return "Resources\\Sound\\Lose.wav"; } } 
         
     }
 }
